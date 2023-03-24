ed964a275b98caed3e780860ee29d767b5b41856
小饼干 <983393186@qq.com>
2023-03-24 23:49:14 +0800

    Update test.env.js

--- config/test.env.js
+++ config/test.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"test"',
-  BASE_API: '"http://121.14.12.105:9797/"'
+  BASE_API: '"http://127.0.0.1:9797/"'
 })
